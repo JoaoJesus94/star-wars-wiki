6de4bb80ce2ea662bbf2029a25b2a01dbf56c4af
JoaoJesus94 <joaojesus_94@hotmail.com>
2022-11-01 10:28:22 +0000

    style: make layout look better

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,17 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
+import { Logo } from './Logo'
 
 export function Layout() {
 	return (
-		<>
-			<span>Shared layout</span>
-			<Outlet />
-		</>
+		<div className="w-screen h-screen">
+			<nav className="w-full flex items-center h-20 px-6 py-4 border-b border-b-slate-100">
+				<Link to={'/'}>
+					<Logo className="h-8" />
+				</Link>
+			</nav>
+			<main className="p-6">
+				<Outlet />
+			</main>
+		</div>
 	)
 }
