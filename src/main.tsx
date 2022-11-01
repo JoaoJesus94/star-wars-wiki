26db79eb4d266a7ffbf7fcc0062bd31c3a87ff0e
JoaoJesus94 <joaojesus_94@hotmail.com>
2022-11-01 12:16:39 +0000

    feat: add redux-persist and make favourites list persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-import { store } from './redux/store'
+import { store, persistor } from './redux/store'
 import { router } from './router'
 import './styles/global.css'
 
@@ -18,10 +19,12 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<QueryClientProvider client={queryClient}>
-				<RouterProvider router={router} />
-				<ReactQueryDevtools initialIsOpen={false} />
-			</QueryClientProvider>
+			<PersistGate loading={null} persistor={persistor}>
+				<QueryClientProvider client={queryClient}>
+					<RouterProvider router={router} />
+					<ReactQueryDevtools initialIsOpen={false} />
+				</QueryClientProvider>
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>,
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,31 @@
+import { combineReducers } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import favouritesSlice from './favouritesSlice'
 
+const persistConfig = {
+	key: 'root',
+	version: 1,
+	storage,
+}
+
+const rootReducer = combineReducers({ [favouritesSlice.name]: favouritesSlice.reducer })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-	reducer: {
-		[favouritesSlice.name]: favouritesSlice.reducer,
-	},
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
