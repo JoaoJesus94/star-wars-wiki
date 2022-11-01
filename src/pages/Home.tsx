14392c843286884d83faeaefd558e0a1b86c3a24
JoaoJesus94 <joaojesus_94@hotmail.com>
2022-11-01 17:39:17 +0000

    feat: add loader when data is loading

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import { usePeople } from '../hooks/usePeople'
 import { CharacterCard } from '../components/CharacterCard'
+import { Loader } from '../components/Loader'
 
 export function Home() {
-	const { data } = usePeople()
+	const { data, isLoading } = usePeople()
+
+	if (isLoading) <Loader />
 
 	return (
 		<ul className="grid gap-4 py-2 justify-center grid-cols-[repeat(auto-fit,_minmax(400px,_max-content))]">
